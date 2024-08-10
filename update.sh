37e6016e14cb48ff87a10d666d638f23b51b7e9d
Tomasz Drwięga <tomusdrw@gmail.com>
2024-08-10 22:58:02 +0200

    Make the script smarter.

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 cd graypaper
+INITIAL="$(git rev-parse HEAD)"
 git pull
+CURRENT="$(git rev-parse HEAD)"
+
+if [ $INITIAL == $CURRENT ];
+then
+  echo "Latest version ($CURRENT) already built."
+  exit 0
+fi
 cd -
 ./scripts/build-html-docker.sh
 
