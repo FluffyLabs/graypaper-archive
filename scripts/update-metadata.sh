4aaf99093ff526ddb466a864211811e681c59eea
Mateusz Sikora <ms1qaz@gmail.com>
2025-12-27 19:47:37 +0100

    add workflow and modify scripts to build GP from main branch

--- scripts/update-metadata.sh
+++ scripts/update-metadata.sh
@@ -2,9 +2,18 @@
 
 set -uex
 
-cd graypaper 
+cd graypaper
 export GP_DATE="$(git show --no-patch --format=%aD)"
+export REAL_HASH="$(git rev-parse HEAD)"
 cd -
 
-jq --arg ver "$VERSION" --arg dat "$GP_DATE" --arg name "$(cat graypaper/VERSION)" '.latest = $ver | .versions += { ($ver) : { name: $name, hash: $ver, date: $dat }}' ./dist/metadata.json > ./dist/temp-metadata.json
+if [ "${1:-}" = "nightly" ]; then
+  jq --arg hash "$REAL_HASH" --arg dat "$GP_DATE" \
+    '.nightly = { name: "nightly", hash: $hash, date: $dat }' \
+    ./dist/metadata.json > ./dist/temp-metadata.json
+else
+  jq --arg ver "$VERSION" --arg dat "$GP_DATE" --arg name "$(cat graypaper/VERSION)" \
+    '.latest = $ver | .versions += { ($ver) : { name: $name, hash: $ver, date: $dat }}' \
+    ./dist/metadata.json > ./dist/temp-metadata.json
+fi
 mv ./dist/temp-metadata.json ./dist/metadata.json
